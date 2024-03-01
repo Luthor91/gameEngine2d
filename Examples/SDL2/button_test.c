8a50992e6f6cd231fa304560e702fb097c4b6d5b
Luc <58238481+Luthor91@users.noreply.github.com>
2024-03-01 16:48:23 +0100

    Add files via upload

--- Examples/SDL2/button_test.c
+++ Examples/SDL2/button_test.c
@@ -25,7 +25,7 @@ int main(int argc, char* argv[]) {
     /****************************
         Bloc de code pour afficher une fenêtre
     ***************************/
-    Window* window = Window_Init("Window Test", 0, 0, 720, 480, "Assets/Image/background1.jpg", 360, 240, 1.0, 0.0); // here
+    Window* window = Window_Init("Window Test", (SDL_Rect){0, 0, 720, 480}, "Assets/Image/background1.jpg", (SDL_Point){360, 240}, 1.0, 0.0); // here
     SDL_Renderer* renderer = Window_GetRenderer(window);
     
     int total_button = 5; // Nombre de bouton = total_button - 1
