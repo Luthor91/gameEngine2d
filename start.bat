3770a65492a2a27c75d1b0f4a891ee8697341a18
Luc <58238481+Luthor91@users.noreply.github.com>
2024-06-05 22:00:30 +0200

    Delete start.bat

--- start.bat
+++
@@ -1,5 +0,0 @@
-SET DISPLAY=:0
-echo SET DISPLAY=:0 >> %userprofile%\.bashrc
-start /B "VcXsrv" "C:\Program Files\VcXsrv\vcxsrv.exe" -multiwindow -clipboard -wgl -ac
-docker build -t gameengine .
-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=host.docker.internal:0 --net=host gameengine
