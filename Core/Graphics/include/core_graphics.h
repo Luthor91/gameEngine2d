601de873704fc6db57ae65b3f5a50e0d359f6905
Luc <58238481+Luthor91@users.noreply.github.com>
2024-06-10 16:37:36 +0200

    Delete Core directory

--- Core/Graphics/include/core_graphics.h
+++
@@ -1,9 +0,0 @@
-#ifndef CORE_GRAPHICS_H
-#define CORE_GRAPHICS_H
-
-#include "window.h"          // Contient la définition de Vector2
-#include "sprite.h" // Contient la définition de MaterialPhysics
-#include "aspect.h"     // Contient la définition de Acceleration
-#include "font.h"            // Contient la définition de Force
-
-#endif // CORE_GRAPHICS_H
--- Core/Graphics/src/tile.c
+++
@@ -1,8 +0,0 @@
-
-#include "../include/tile.h"
-
-
-void Tile_Render(SDL_Renderer* renderer, int id, int x, int y) {
-
-    return;
-}
